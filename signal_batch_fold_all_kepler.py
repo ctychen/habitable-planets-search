9f23dd9b8eeab1b34a9440ce0f53804d3277314a
CT Chen <34198336+ctychen@users.noreply.github.com>
2020-11-07 20:38:26 -0800

    Code for folding all targets from kepler catalog

---
+++ signal_batch_fold_all_kepler.py
@@ -0,0 +1,189 @@
+import numpy as np
+import os
+import kepler_utils
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import subprocess
+import pandas as pd
+from subprocess import call
+import re
+
+# hsps = pd.read_excel("/media/rd3/cchen/cchen/gpu_codes_new/hsp_60_400_days_v2.xlsx")
+# kepler_id = np.array(hsps['kic'].tolist())
+# tce_period = np.array(hsps['period (d)'].tolist())
+# tce_duration = np.array(hsps['transit_dur (h)'].tolist())
+# # hsp_depth = hsps['transit_depth (ppm)'].tolist()
+# # hsp_smaxis = hsps['sm_axis (au)'].tolist()
+# tce_t0 = np.array(hsps['t0 (bjd)'].tolist())
+# # hsp_incl = hsps['incl (deg)'].tolist()
+# # hsp_eccen = hsps['eccen'].tolist()
+# KOI_id = np.array(hsps['koi'].tolist())
+# cc = tce_duration/(tce_period/4096*24)
+# print(cc.min())
+
+table = pd.read_excel('KeplerCumulative.xlsx')
+
+all_kics = table['kepid'][:]
+all_kepoi_temp = table['kepoi_name'][:]
+# all_t0_catalog_bjd = table['koi_time0'][:]
+# all_period_catalog = table['koi_period'][:]
+# all_duration_catalog = table['koi_duration'][:]
+all_status = table['koi_disposition'][:]
+
+kepler_id = all_kics
+
+
+
+#read the catalog infomation.
+# catalog = np.loadtxt('hps_catalog.txt',dtype='str',skiprows=1)
+
+# #100 days ~ 500 days bin_time = 0.5, folding step: 1hr.
+# #kepid	kepname	period_days	duration	depth	srad	smass	rprs
+
+# kepler_id=catalog[:,0]
+# kepler_name = catalog[:,1]
+# tce_period=catalog[:, 2].astype(np.float64)
+# tce_duration = catalog[:,3].astype(np.float64)
+# print(kepler_id)
+# print(tce_period)
+# print(tce_period)# *24.0)
+# print(tce_duration/(tce_period/4096*24))
+# cc = tce_duration/(tce_period/4096*24)
+# print(cc.min())
+# print(tce_period/(tce_duration/24.0/3.0))
+
+# period_search_array = np.linspace(100,500,38400)
+# period_search_array = np.linspace(60,500,44000)
+period_search_array = np.linspace(100,200,44000)
+np.savetxt('search_array_hps.txt',period_search_array.T)
+
+# period_search_array = np.linspace(60,400,38400)
+
+'''
+plt.plot(tce_period, tce_duration/(tce_period*24.0/256),color='red', marker='.', linestyle='None')
+plt.xlabel('Period (Days)')
+plt.ylabel('Duration (Pixels)')
+plt.show()
+'''
+def mean_fold(time_array, flux_array, period_id, t0, bin_num, bin_size):
+
+
+    time_array = kepler_utils.phase_fold_time(time_array, period_id, t0)
+    sorted_i = np.argsort(time_array)
+    flux = flux_array[sorted_i]
+    time = time_array[sorted_i]
+    flux_out = np.zeros(bin_num)
+    time_out = np.zeros(bin_num)
+
+    for kk in np.arange(bin_num):
+        idx_l = int(kk*bin_size)
+        idx_u = int((kk+1)*bin_size)
+        flux_out[kk] = np.mean(flux[idx_l:idx_u])
+        time_out[kk] = np.mean(time[idx_l:idx_u])
+
+    return time_out, flux_out
+
+
+def get_numbers_from_filename(filename):
+    return re.search(r'\d+', filename).group(0)
+
+dir_list1 = os.listdir('/media/rd3/cchen/cchen/hsp_gpu_fold/all_kepler_folded/')
+print(len(dir_list1))
+# print(dir_list1[0])
+
+def find_first(item, vec):
+    """return the index of the first occurence of item in vec"""
+    for i in range(len(vec)):
+        if item == vec[i]:
+            return i
+    return -1
+
+
+
+
+for id in np.arange(kepler_id.size):
+    print(id)
+#     id = 3
+    kid = int(kepler_id[id])
+    kic = int(get_numbers_from_filename(dir_list1[id]))
+    kic_index = find_first(int(get_numbers_from_filename(dir_list1[id])), kepler_id)
+    print(kid)
+    #file_name = '/media/etdisk2/Yinan_Zhao/gpu_code/kepler_detrend_new/'+str(kid)+'.npz'
+#     file_name = '/media/rd3/surveys/kepler/final_normalized_lcs/KIC'+str(kepler_id[id])+'_withfit_long_v1.npz'
+#     file_name = '/media/rd3/cchen/cchen/kepler_ml_v3/norm_lcs/' + str(kepler_id[id]) + '_transitsmasked.npz'
+
+#     file_name = '/media/rd3/cchen/cchen/kepler_ml_v3/norm_lcs/legit/' + str(kepler_id[id]) + '_nofilt.npz'
+
+#9002278_1_filt.npz
+
+    if (all_status[kic_index] == 'CONFIRMED'):
+        
+#     file_name = '/media/rd3/cchen/cchen/kepler_ml_v3/norm_lcs/v2/hsps_v2/' + str(kepler_id[id]) + '_filt.npz'
+
+        file_name = '/media/rd3/cchen/cchen/kepler_ml_v3/norm_lcs/v2/all_kepler/' + str(kepler_id[id]) + '_filt.npz'
+
+    #     file_name = '/media/rd3/surveys/kepler/normalized_lcs/KIC'+str(kepler_id[id])+'_long_v1.npz'
+
+        data_frame = np.load(file_name)
+        time = data_frame['time']
+        flux = data_frame['flux']
+    #     spline = data_frame['fit']
+        detrend = flux#/spline
+    #     period = tce_period[id]
+
+    #     print(period/0.5*24.0, tce_duration[id])
+
+        #plt.plot(time,detrend,marker='.', linestyle='None',color='black')
+        #plt.show()
+        #exit()
+
+
+        no_data = flux.size
+        no_bin = 8192#4096
+        no_sum = np.floor(no_data/no_bin)
+
+        resize = int(no_bin* no_sum)
+        time_array = np.ones(resize)
+        detrend_array = np.ones(resize)
+
+        time_array = time[:resize]
+        detrend_array = detrend[:resize]
+
+
+    #     index_min = np.argmin(np.abs(period_search_array-period))
+
+        '''
+        print(period*24.0/0.5, period_search_array[index_min]*24.0/0.5)
+        pix_out1, out1 = mean_fold(time_array, detrend_array, period, 0.0, no_bin, no_sum)
+        pix_out2, out2 = mean_fold(time_array, detrend_array, period_search_array[index_min], 0.0,no_bin, no_sum)
+        plt.plot(out1,marker='.', linestyle='None',color='red')
+        plt.plot(out2,marker='.', linestyle='None',color='black')
+        plt.show()
+        '''
+        sigma = np.std(detrend_array)/no_sum**0.5
+
+
+        #exit()
+        np.savetxt('kepler_buffer_hps.txt', np.array([time_array, detrend_array]).T)
+
+        gpu_cmd = "CUDA_VISIBLE_DEVICES=0 ./test_hps "+str(resize)+" "+str(kepler_id[id])
+        print(gpu_cmd)
+        os.system(gpu_cmd)
+    #     saved_file = '/media/rd3/cchen/cchen/hsp_gpu_fold/all_kepler_folded/data_'+str(kepler_id[id])+'_hps.bin'
+    #     p_num = 44000 #38400
+    #     p_num = 32640
+    #     data_cube= np.fromfile(saved_file, dtype='double').reshape(p_num,no_bin)
+        #exit()
+        print('done')
+        #out = mean_fold(time_array, detrend_array, period, 0.0)
+        #out = mean_fold(time_array, detrend_array, period_search_array[index_min], 0.0)
+    #     plt.figure()
+    #     plt.plot(data_cube[index_min,:],marker='.', linestyle='None',color = 'black', label = 'GPU')
+    #     #plt.plot(out, color = 'red', label = 'CPU')
+    #     plt.legend(loc='best', fontsize = 9)
+    #     plt.title('kepler ID: '+str(kid)+' period:'+str(period))
+    #     plt.savefig('/media/rd3/cchen/cchen/hsp_gpu_fold/figures/v3/' + str(kid)+'.png')
+    #     plt.show()
+    #     plt.close()
+
+#         exit()
